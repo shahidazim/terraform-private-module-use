de44b7a26beeeb4025c2d93b91cbbc195c7ee55c
shahidazim <Shahid.Azim@asos.com>
2021-12-30 15:39:44 +0000

    fixed module name

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "storage_account_name" {
-  value = module.static_website.storage_account_name
+  value = module.static_webapp.storage_account_name
 }
 
 output "website_endpoint" {
-  value = module.static_website.website_endpoint
+  value = module.static_webapp.website_endpoint
 }
