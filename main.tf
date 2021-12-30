b9d35167e6165abd2b7ac44ebeb23f1b96dbd8d9
shahidazim <Shahid.Azim@asos.com>
2021-12-30 15:27:53 +0000

    initial commit with module usage

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Terraform configuration
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_pet" "petname" {
+  length    = 2
+  separator = ""
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
+}
+
+module "static-webapp" {
+  source                   = "app.terraform.io/shahidazim/static-webapp/azure"
+  version                  = "1.0.0"
+  resource_group_name      = azurerm_resource_group.example.name
+  storage_account_location = var.location
+  storage_account_name     = random_pet.petname.id
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "storage_account_name" {
+  value = module.static_website.storage_account_name
+}
+
+output "website_endpoint" {
+  value = module.static_website.website_endpoint
+}
